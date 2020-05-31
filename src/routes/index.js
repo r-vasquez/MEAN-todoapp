dbb33abf179b7f2ec00fb05da62d996524526b32
Rogger <rvasque3@gmail.com>
2020-05-31 18:17:26 -0500

    setting up bower and installing FE Dependencies

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,9 @@
 const { Router } = require('express')
 const router = Router()
+const path = require('path')
 
-router.get('/', async (req, res) => {
-  const response = {
-    message: 'hola desde Index'
-  }
-
-  res.json(response)
+router.get('*', async (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/views/index.html'))
 })
 
 module.exports = router
